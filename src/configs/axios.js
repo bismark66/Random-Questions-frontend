f0b345d4e0b31ed11b58d8e7eda793b314399234
Bismark <agyarebismark6@gmail.com>
2023-09-27 02:59:14 +0000

    commented out axios instance in axios.js

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,8 +1,8 @@
 /** @format */
-import axios from "axios";
+// import axios from "axios";
 
-const instance = axios.create({
-  base_url: "https://the-trivia-api.com/api/questions?limit=1&difficulty=easy",
-  headers: { "content-type": "application/json" },
-});
-export default instance;
+// const instance = axios.create({
+//   base_url: "https://the-trivia-api.com/api/questions?limit=1&difficulty=easy",
+//   headers: { "content-type": "application/json" },
+// });
+// export default instance;
