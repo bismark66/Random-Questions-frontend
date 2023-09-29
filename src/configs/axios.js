131ff0cacc88fada20308759111c3487713ac3cc
Bismark <agyarebismark6@gmail.com>
2023-09-29 23:26:19 +0000

    removed axios.js file

--- src/configs/axios.js
+++
@@ -1,8 +0,0 @@
-/** @format */
-// import axios from "axios";
-
-// const instance = axios.create({
-//   base_url: "https://the-trivia-api.com/api/questions?limit=1&difficulty=easy",
-//   headers: { "content-type": "application/json" },
-// });
-// export default instance;
